e2d921383642559d43a87183dde119eafe4f9641
Loisployce <76707230+Loisployce@users.noreply.github.com>
2023-09-12 12:11:34 +0200

    Delete SoftwareArchitektur - ProxyServer directory

--- SoftwareArchitektur - ProxyServer/src/IChatRoom.java
+++
@@ -1,5 +0,0 @@
-public interface IChatRoom {
-    public void sendMessage(String message, IChatter chatter);
-    public void addChatter(IChatter chatter);
-    public void exitChatter(IChatter chatter);
-}
